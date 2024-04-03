386078e7ad4b688d2b2d694d68ecd55546e8a403
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-04-03 19:56:21 +0530

    Created Prompt model

---
+++ lib/models/prompt.model.ts
@@ -0,0 +1,26 @@
+import { Document, Model, Schema, model, models } from "mongoose";
+
+export interface IPrompt extends Document {
+  prompt: string;
+  type: "Conversation" | "Image" | "Video" | "Music" | "Code";
+  response: string;
+  media: string[];
+  userId: string;
+  createdAt: Date;
+}
+
+const promptSchema = new Schema<IPrompt>({
+  prompt: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["Conversation", "Image", "Video", "Music", "Code"],
+  },
+  response: String,
+  media: [String],
+  userId: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Prompt: Model<IPrompt> = models?.Prompt || model("Prompt", promptSchema);
+
+export default Prompt;
